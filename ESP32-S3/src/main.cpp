e4265cbc0f9fcd67dea4de947e02bcdf57de81a3
Kuzuryu <71879159+Kuzuryuz@users.noreply.github.com>
2024-11-28 16:08:54 +0700

    Add line notification

--- ESP32-S3/src/main.cpp
+++ ESP32-S3/src/main.cpp
@@ -1,17 +1,20 @@
-#define BLYNK_TEMPLATE_ID "TMPL6lEgwGCl1"
-#define BLYNK_TEMPLATE_NAME "Project"
-#define BLYNK_AUTH_TOKEN "cGFTkOIASPTcT4cgfxL6jOWcRcMiP8ZX"
+// #define BLYNK_TEMPLATE_ID "TMPL6lEgwGCl1"
+// #define BLYNK_TEMPLATE_NAME "Project"
+// #define BLYNK_AUTH_TOKEN "cGFTkOIASPTcT4cgfxL6jOWcRcMiP8ZX"
 
-#define BLYNK_PRINT Serial
+// #define BLYNK_PRINT Serial
 
 #include <WiFi.h>
 #include <WiFiClient.h>
-#include <BlynkSimpleEsp32.h>
+#include <HTTPClient.h>
+// #include <BlynkSimpleEsp32.h>
 #include <Arduino.h>
 #include <BH1750FVI.h>
 
 char ssid[] = "";
 char pass[] = "";
+
+const char* lineToken = "EtXxXHJfnpg0UVtf5oj1X9sZkMTuoj7UtYNk9z2hAPP";
 
 // Define pins and sensors
 #define RX (35)
@@ -20,10 +23,8 @@ char pass[] = "";
 #define relayPin (21)
 BH1750FVI LightSensor(BH1750FVI::k_DevModeContLowRes);
 
-// Define variable to record recieved data from ESP32 DEVKIT V1
+// Define variable to store data from sensor and sensor node
 String receivedData;
-
-// Define Variable to record data from sensors
 float humidity, temperature;
 int gasData;
 uint16_t lux;
@@ -33,8 +34,34 @@ int motionState = LOW;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 750;
 
+// Function to send notification to line
+void sendLineNotify(String message) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    String url = "https://notify-api.line.me/api/notify";
+    http.begin(url);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    http.addHeader("Authorization", "Bearer " + String(lineToken));
+    String body = "message=" + String(message);
+    int httpResponseCode = http.POST(body);
+    if (httpResponseCode > 0) {
+      Serial.print("HTTP Response Code: ");
+      Serial.println(httpResponseCode);
+      Serial.println(http.getString());
+      Serial.println("------------------------------------------------------------");
+    } else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+      Serial.println("------------------------------------------------------------");
+    }
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+}
+
 // Extract recieved data from ESP32 DEVKIT V1
-void processReceivedData(String data) {
+void sendDataToCloudPlatform(String data) {
   if (data.startsWith("Humidity:")) {
     // Get humidity data
     int hIndex = data.indexOf(':') + 2;
@@ -59,10 +86,18 @@ void processReceivedData(String data) {
   }
 
   // Send to Blynk
-  Blynk.virtualWrite(V1, humidity);
-  Blynk.virtualWrite(V2, temperature);
-  Blynk.virtualWrite(V3, gasData);
-  Blynk.virtualWrite(V4, lux);
+  // Blynk.virtualWrite(V1, humidity);
+  // Blynk.virtualWrite(V2, temperature);
+  // Blynk.virtualWrite(V3, gasData);
+  // Blynk.virtualWrite(V4, lux);
+
+  // If high temperature and smoke detection
+  if (temperature > 45.00) {
+    Serial.println("------------------------------------------------------------");
+    Serial.println("Fire Alarm Detected!");
+    String notiMessage = "ตรวจพบควันและอุณหภูมิสูง (" + String(temperature) + "°C)";
+    sendLineNotify(notiMessage);
+  }
 }
 
 void setup() {
@@ -74,17 +109,25 @@ void setup() {
 
   LightSensor.begin();  
 
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  Serial.print("Connecting to WiFi...");
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("\nConnected to WiFi!");
+
+  // Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
 
 void loop() {
-  Blynk.run();
+  // Blynk.run();
 
   // Read data from sensor node (ESP32 DEVKIT V1)
   if (Serial1.available()) {
     char c = Serial1.read();
     if (c == '\n') {
-      processReceivedData(receivedData);
+      sendDataToCloudPlatform(receivedData);
       receivedData = "";
     } else {
       receivedData += c;
@@ -107,7 +150,7 @@ void loop() {
           Serial.println("------------------------------------------------------------");
         }
         digitalWrite(relayPin, !digitalRead(relayPin));
-        Blynk.virtualWrite(V0, digitalRead(relayPin));
+        // Blynk.virtualWrite(V0, digitalRead(relayPin));
       }
       lastDebounceTime = millis();
     }
